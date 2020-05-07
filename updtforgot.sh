aee6d7a049bee0bca7a9d18e61e1e5f0fb5bcee9
christian-hawk <eland.christian@gmail.com>
2020-05-07 02:50:06 -0300

    small corrections

--- updtforgot.sh
+++ updtforgot.sh
@@ -38,7 +38,7 @@ function ctrl_c()
 			echo "updating PY..."
 			scp $PY_WORKING $REMOTE_SERVER_USERNAME@$REMOTE_GLUU_SERVER:interceptsh
 			ssh -t $REMOTE_SERVER_USERNAME@$REMOTE_GLUU_SERVER sudo cp ./interceptsh/*.* $PY_GLUU_CHMOD
-			wait 1s
+			sleep 1s
 			ssh -t $REMOTE_SERVER_USERNAME@$REMOTE_GLUU_SERVER sudo rm ./interceptsh/*.*
 
 			echo "Files *.py updated on gluu-server chmod"
@@ -52,7 +52,7 @@ function ctrl_c()
 			date -d now 
 			sudo cp /home/christian/gluu-repos/oxAuthRepo/Server/integrations/forgot_password/*.py /opt/gluu-server/custom/.
 			echo "Files *.py updated on gluu-server chmod"
-			wait 2
+			sleep 2s
 			sudo cp /home/christian/gluu-repos/oxAuthRepo/Server/src/main/webapp/auth/forgot_password/*.xhtml /opt/gluu-server/opt/gluu/jetty/oxauth/custom/pages/auth/forgot_password/.
 			echo "Files *.xhtml updated on gluu-server chmod"
 			sleep 1
